ba76e858d0d9ecabbafb9ef71d696f403ad2b41f
Ben Prescott <bcprescott@outlook.com>
2026-02-02 21:29:53 -0600

    updated cached

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VDWFB+IGTDh/6HWMez3+q7xvV88Tgr87/BXYvcTefQc=\"\n}"
--- src/lib/utils.ts
+++
@@ -1,5 +0,0 @@
-export const calculate1RM = (weight: number, reps: number): number => {
-    if (reps === 1) return weight
-    // Epley Formula
-    return Math.round(weight * (1 + reps / 30))
-}
